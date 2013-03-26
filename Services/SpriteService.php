56ae5bf898503dab8b45ca7248623c6b407f99f4
pl4n7tis <sd-27105@dedibox.fr>
2013-03-26 11:13:58 +0100

    replace - in options name, doesn't work with yaml

--- Services/SpriteService.php
+++ Services/SpriteService.php
@@ -2,7 +2,6 @@
  
 namespace Sphax\SpriteBundle\Services;
 
-use Sphax\SpriteBundle\SpriteConf\SpriteConf;
 use Sphax\SpriteBundle\Exception\DirectoryException;
 use Sphax\SpriteBundle\Exception\SpriteException;
 use Symfony\Component\Filesystem\Filesystem;
@@ -20,7 +19,6 @@ class SpriteService implements SpriteServiceInterface
     /**
      * @var SpriteConfInterface
      */
-    protected $spriteConf;
     private $config;
 
 
@@ -47,6 +45,11 @@ class SpriteService implements SpriteServiceInterface
     public function getSpriteList()
     {
         $spriteList = $this->getConfig();
+        foreach ($spriteList as $key => $value) {
+            foreach ($value['options'] as $keyOptions => $valueOptions) {
+                str_replace('-', '_', $keyOptions);
+            }
+        }
         if (empty($spriteList)) {
             throw new SpriteException('No sprite configuration found.');
         }
@@ -63,6 +66,8 @@ class SpriteService implements SpriteServiceInterface
         $filesystem = new Filesystem();
 
         $spriteList = $this->getSpriteList();
+        print_r($spriteList);
+        die();
         foreach ($spriteList as $key => $spriteInfo) {
             if ($filesystem->exists($spriteInfo['sourceSpriteImage']) === false) {
                 throw new DirectoryException('Directory doesn\'t exist');
@@ -133,21 +138,21 @@ class SpriteService implements SpriteServiceInterface
                 (!empty($spriteInfo['options']['ordering']) ? ' --ordering=' . $spriteInfo['options']['ordering'] : '') .
                 (!empty($spriteInfo['options']['margin']) ? ' --margin=' . $spriteInfo['options']['margin'] : '') .
                 (!empty($spriteInfo['options']['namespace']) ? ' --namespace=' . $spriteInfo['options']['namespace'] : '') .
-                (!empty($spriteInfo['options']['sprite-namespace']) ? ' --sprite-namespace=' . $spriteInfo['options']['sprite-namespace'] : '') .
+                (!empty($spriteInfo['options']['sprite_namespace']) ? ' --sprite-namespace=' . $spriteInfo['options']['sprite_namespace'] : '') .
                 (!empty($spriteInfo['options']['separator']) ? ' --separator=' . $spriteInfo['options']['separator'] : '') .
-                (!empty($spriteInfo['options']['global-template']) ? ' --global-template=' . $spriteInfo['options']['global-template'] : '') .
-                (!empty($spriteInfo['options']['each-template']) ? ' --each-template=' . $spriteInfo['options']['each-template'] : '') .
+                (!empty($spriteInfo['options']['global_template']) ? ' --global-template=' . $spriteInfo['options']['global_template'] : '') .
+                (!empty($spriteInfo['options']['each_template']) ? ' --each-template=' . $spriteInfo['options']['each_template'] : '') .
                 (!empty($spriteInfo['options']['margin']) ? ' --margin=' . $spriteInfo['options']['margin'] : '') .
                 ($spriteInfo['options']['png8'] != false ? ' --png8 ' : '') .
-                (!empty($spriteInfo['options']['ignore-filename-paddings']) ? ' --ignore-filename-paddings ' : '') .
+                (!empty($spriteInfo['options']['ignore_filename_paddings']) ? ' --ignore-filename-paddings ' : '') .
                 ($spriteInfo['options']['debug'] != false ? ' --debug ' : '') .
                 ($spriteInfo['options']['optipng'] != false ? ' --optipng ' : '') .
                 ($spriteInfo['options']['cachebuster'] != false ? ' --cachebuster ' : '') .
-                ($spriteInfo['options']['follow-links'] != false ? ' --follow-links ' : '') .
+                ($spriteInfo['options']['follow_links'] != false ? ' --follow-links ' : '') .
                 ($spriteInfo['force'] != false ? ' --force ' : '') .
-                ($spriteInfo['options']['no-img'] != false ? ' --no-img ' : '') .
-                ($spriteInfo['options']['no-css'] != false ? ' --no-css ' : '') .
-                ($spriteInfo['options']['cachebuster-filename'] != false ? ' --cachebuster-filename ' : '') .
+                ($spriteInfo['options']['no_img'] != false ? ' --no-img ' : '') .
+                ($spriteInfo['options']['no_css'] != false ? ' --no-css ' : '') .
+                ($spriteInfo['options']['cachebuster_filename'] != false ? ' --cachebuster_filename ' : '') .
                 (!empty($spriteInfo['options']['optipngpath']) ? ' --optipng --optipngpath=' . $spriteInfo['options']['optipngpath'] : '') .
                 $spriteInfo['sourceSpriteImage'] . ' ' .
                 $spriteInfo['outputSpriteImage'],
