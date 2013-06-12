f5df5fba4c6c1beea05bbdb24e12f527d5612d86
pl4n7tis <sd-27105@dedibox.fr>
2013-06-12 15:12:39 +0200

    test

---
+++ src/SphaxSprite/Exception/DirectoryException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sphax\SphaxSprite\Exception;
+
+
+/**
+ * DirectoryException
+ */
+class DirectoryException extends \Exception
+{
+}
---
+++ src/SphaxSprite/Services/SpriteServiceInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sphax\SphaxSprite\Services;
+
+/*
+*
+* Define a classic confirguration for a sprite
+*
+*/
+interface SpriteServiceInterface
+{
+    /**
+     * liste des sprites
+     * 
+     */
+    public function getSpriteList();
+
+
+    /**
+     * generation du sprite
+     * 
+     */
+    public function generateSprite();
+
+    /**
+     * generation du sprite
+     * 
+     */
+    public function generateOneSprite($spriteName);
+
+}
